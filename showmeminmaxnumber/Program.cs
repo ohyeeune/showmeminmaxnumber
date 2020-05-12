c545c25103ec868ce7df81107d299a8a20f0a15d
ohyeeune <s2018s33@e-mirim.hs.kr>
2020-05-12 13:00:44 +0900

    5개 입력 받아서 최댓값, 최솟값 구하기

--- showmeminmaxnumber/Program.cs
+++ showmeminmaxnumber/Program.cs
@@ -10,6 +10,25 @@ namespace showmeminmaxnumber
     {
         static void Main(string[] args)
         {
+            int[] num = new int[5];
+            int max = 0, min = 0;
+            for(int i=0; i<num.Length; i++)
+            {
+                Console.Write("숫자를 입력해주세요: ");
+                num[i] = int.Parse(Console.ReadLine());
+            }
+
+            max = num[0]; min = num[0];
+            for(int i = 0; i<num.Length; i++)
+            {
+                if (max < num[i]) max = num[i];
+                else if (min > num[i]) min = num[i];
+            }
+            
+
+            Console.WriteLine("---------------------------");
+            Console.WriteLine("가장 큰 수 : " + max);
+            Console.WriteLine("가장 작은 수 : " + min);
         }
     }
 }
